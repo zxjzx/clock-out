2be83ec83b088dd5912ddffb1f29b0b8b46b7555
jane <jane@interbiztech.cn>
2019-06-11 15:28:39 +0800

    add tip-off

--- server/mysql/tip-off.js
+++ server/mysql/tip-off.js
@@ -2,19 +2,51 @@ const express = require('express')
 const router = express.Router()
 const db = require('./db.js')
 router.use('/getTipoffList', (req, res) => {
-  let sql = "select t.id,t.content,t.created,u.id as outUserId,u.userName as outUsername,o.id as reporUserId,o.username as reportName\
+
+  let currentPage = req.body.currentPage
+  let pageSize = req.body.pageSize
+  let startSize = (currentPage - 1) * pageSize
+
+  let whereSql = 'where 1 = 1'
+  let outid = req.body.outid
+  let reporterid = req.body.reporterid
+  let startTime = req.body.startTime
+  let endTime = req.body.endTime
+
+  if (outid) {
+    whereSql += ' and t.outid=' + outid
+  }
+  if (reporterid) {
+    whereSql += ' and t.reporterid=' + reporterid
+  }
+
+  if (startTime) {
+    whereSql += ' and t.created >= ' + startTime
+  }
+  if (endTime) {
+    whereSql += ' and t.created <= ' + endTime
+  }
+
+  let sql = 'select SQL_CALC_FOUND_ROWS\
+  t.id,t.content,t.created,u.id as outUserId,u.userName as outUsername,o.id as reporUserId,o.username as reportName\
   FROM tipoff_record t\
   left join user u on u.id=t.outid\
   left join user o on o.id=t.reporterid\
-  ORDER BY t.created desc"
+  ' + whereSql + '\
+  ORDER BY t.created desc limit ' + startSize + ',' + pageSize + '; \
+  SELECT FOUND_ROWS() as total ;'
   db.query(sql, (err, rows) => {
     if (err) {
-      req.send(err)
+      res.send(err)
     } else {
+      let data = {
+        result: rows[0],
+        total: rows[1][0].total
+      }
       let result = {
         code: 0,
         message: 'OK',
-        data: rows,
+        data: data,
         status: true
       }
       res.send(result)
--- src/views/tipoff-record.vue
+++ src/views/tipoff-record.vue
@@ -1,53 +1,122 @@
 <template>
-  <el-row>
-    <el-col :span="16">
-      <el-table
-        :data="dataList"
-        style="width: 100%">
-        <el-table-column
-          type="index">
-        </el-table-column>
+  <div>
+    <el-row>
+      <el-form :label-position="labelPosition" label-suffix=" :" label-width="160px" :inline="true" :model="tableForm">
+        <el-form-item label="Reportee">
+          <userlist-select :value.sync="tableForm.reportee" @change="searchSubmit"/>
+        </el-form-item>
+        <el-form-item label="Reporter">
+          <userlist-select :value.sync="tableForm.reporter" @change="searchSubmit"/>
+        </el-form-item>
+        <el-form-item label="Time Range">
+          <el-date-picker
+            v-model="tableForm.time"
+            type="datetimerange"
+            range-separator="-"
+            start-placeholder="start date"
+            end-placeholder="end date"
+            align="right"
+            @change="searchSubmit">
+          </el-date-picker>
+        </el-form-item>
 
-        <el-table-column
-          prop="created"
-          label="created">
-        </el-table-column>
-        <el-table-column
-          prop="content"
-          label="content">
-        </el-table-column>
-
-        <el-table-column
-          prop="outUsername"
-          label="被举报人">
-        </el-table-column>
-        <el-table-column
-          prop="reportName"
-          label="举报人">
-        </el-table-column>
-      </el-table>
-    </el-col>
-  </el-row>
+        <el-form-item>
+          <el-button type="primary" @click="searchSubmit">Search</el-button>
+          <el-button @click="resetForm">Reset</el-button>
+        </el-form-item>
+      </el-form>
+    </el-row>
+    <el-row>
+      <el-col :span="16">
+        <el-table
+          :data="dataList"
+          style="width: 100%">
+          <el-table-column
+            type="index">
+          </el-table-column>
+          <el-table-column
+            prop="content"
+            label="content">
+          </el-table-column>
+          <el-table-column
+            prop="outUsername"
+            label="Reportee">
+          </el-table-column>
+          <el-table-column
+            prop="reportName"
+            label="Reporter">
+          </el-table-column>
+          <el-table-column
+            prop="createdFormat"
+            label="created">
+          </el-table-column>
+        </el-table>
+        <el-pagination
+          background
+          layout="total,prev, pager, next"
+          @current-change="handleCurrentChange"
+          :current-page="page.currentPage"
+          :page-size="page.pageSize"
+          :total="page.total">
+        </el-pagination>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
+  import UserlistSelect from '../components/userlist-select'
+
   export default {
     name: 'tipoff-record',
+    components: { UserlistSelect },
     data () {
       return {
-        dataList: []
+        dataList: [],
+        labelPosition: 'right',
+        tableForm: {},
+        page: {
+          currentPage: 1,
+          pageSize: 10,
+          total: 0
+        }
       }
     },
     created () {
-      this.tipoffList()
+      this.searchSubmit()
     },
-    methods:{
-      tipoffList(){
-        this.$http.post('getTipoffList').then(res => {
+    methods: {
+      searchSubmit () {
+        let obj = {
+          currentPage: this.page.currentPage,
+          pageSize: this.page.pageSize,
+          reporterid: this.tableForm.reporter,
+          outid: this.tableForm.reportee,
+          startTime: this.tableForm.time ? JSON.stringify(this.$timeFormat(this.tableForm.time[0])) : null,
+          endTime: this.tableForm.time ? JSON.stringify(this.$timeFormat(this.tableForm.time[1])) : null,
+        }
+        console.log(this.tableForm)
+
+        this.$http.post('getTipoffList', obj).then(res => {
           console.log(res.data)
-          this.dataList = res.data
+          this.dataList = res.data.result.map(item => {
+            item.createdFormat = this.$timeFormat(new Date(item.created))
+            return item
+          })
+          this.dataList = res.data.result
+          this.page.total = res.data.total
         })
-      }
+      },
+      resetForm () {
+        this.tableForm = {}
+        this.searchSubmit()
+      },
+      //分页查询打卡记录
+      handleCurrentChange (val) {
+        this.page.currentPage = val
+        this.searchSubmit()
+        console.log(`当前页: ${val}`)
+      },
     }
   }
 </script>
