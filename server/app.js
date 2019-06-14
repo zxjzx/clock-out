88f9f2006244370bd431187dedf2b97c15a8f8f9
jane <jane@interbiztech.cn>
2019-06-14 20:35:50 +0800

    add proxy 反向代理

--- server/app.js
+++ server/app.js
@@ -11,17 +11,6 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))//解析request中body的urlencoded字符
 
-app.use('*', (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild')
-  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS')
-  if (req.method === 'OPTIONS') {
-    res.send(200) // 意思是，在正常的请求之前，会发送一个验证，是否可以请求。
-  } else {
-    next()
-  }
-})
-
 //数据库
 const user = require('./mysql/user')
 const setting = require('./mysql/setting')
@@ -30,15 +19,14 @@ const tipOff = require('./mysql/tip-off')
 app.use('/', user)
 app.use('/', setting)
 app.use('/', clockOut)
-app.use('/', tipOff)
+app.use('/', tipOff);
 
-/*app.get('*', (req, res) => {
-  // res.send("错误的get请求")
-  res.status(404).json("错误的get请求")
-})
-app.post('*', (req, res, err) => {
-  res.status(400).json("错误的post请求")
-})*/
+app.get('/', function (req, res) {
+  res.send(403)
+});
+app.post('/', function (req, res) {
+  res.send(403)
+});
 
 const server = http.createServer(app)
 
