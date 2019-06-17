c873938ec7e786f0d621ce338ad8ee4c871a36f1
jane <jane@interbiztech.cn>
2019-06-17 14:47:37 +0800

    fix test http

--- server/app.js
+++ server/app.js
@@ -11,6 +11,17 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))//解析request中body的urlencoded字符
 
+app.use('*', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild')
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS')
+  if (req.method === 'OPTIONS') {
+    res.send(200) // 意思是，在正常的请求之前，会发送一个验证，是否可以请求。
+  } else {
+    next()
+  }
+})
+
 //数据库
 const user = require('./mysql/user')
 const setting = require('./mysql/setting')
--- src/utils/http.js
+++ src/utils/http.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // Set config defaults when creating the instance
-axios.defaults.baseURL = '/api'
+axios.defaults.baseURL = 'http://127.0.0.1:3000'
 
 export default {
   get (url, param) {
