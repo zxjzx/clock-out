b01918cb1cdae277dc7ad6750401692fe6e87dca
jane <jane@interbiztech.cn>
2019-06-25 16:03:02 +0800

    test proxy

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://127.0.0.1:4000',
         ws: true,
         changeOrigin: true,
         pathRewrite: {
